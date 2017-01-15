7ee8a3ca87b0bcccd1a8dba4e4af408e2081ee01
Tiant <TiantPlay@outlook.com>
2017-01-15 14:26:28 +0000

    modified config/app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'wwk88kY3csOjmOtxdfEi1236cU8AjJ5F'),
 
     'cipher' => 'AES-256-CBC',
 
